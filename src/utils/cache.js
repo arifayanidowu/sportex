7b2f7182b1b9188e4b08605f2a9b121a37a1d06a
Arifayan Idowu Olatubosun <arifayanidowu2@yahoo.com>
2020-10-12 01:47:02 +0100

    client redis config

--- src/utils/cache.js
+++ src/utils/cache.js
@@ -2,6 +2,10 @@ const ExpressRedisCache = require("express-redis-cache");
 
 const cache = ExpressRedisCache({
   expire: 10,
+  host:
+    process.env.NODE_ENV === "production"
+      ? process.env.REDIS_URL
+      : "http://localhost",
 });
 
 module.exports = cache;
